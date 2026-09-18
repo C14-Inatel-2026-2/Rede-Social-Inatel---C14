b01901012daf60d367914e3e70613c952037fe46
LCZW973 <leonardoczanin@gmail.com>
2026-09-18 18:44:33 -0300

    feat: Cria classe ClienteHandler e seu construtor

---
+++ src/main/java/br/inatel/redesocial/cliente/ClienteHandler.java
@@ -0,0 +1,13 @@
+package br.inatel.redesocial.cliente;
+
+public class ClienteHandler {
+
+    private int porta;
+    private String host;
+    public  ClienteHandler(int porta , String host)
+    {
+        this.porta=porta;
+        this.host=host;
+    }
+
+}
