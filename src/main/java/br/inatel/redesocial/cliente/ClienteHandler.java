030283a5837fc683cd191b623601460f4e9d9e6f
LCZW973 <leonardoczanin@gmail.com>
2026-09-18 18:48:41 -0300

    feat: cria gerenciador de chat do cliente

--- src/main/java/br/inatel/redesocial/cliente/ClienteHandler.java
+++ src/main/java/br/inatel/redesocial/cliente/ClienteHandler.java
@@ -1,5 +1,8 @@
 package br.inatel.redesocial.cliente;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClienteHandler {
 
     private int porta;
@@ -10,4 +13,22 @@ public class ClienteHandler {
         this.host=host;
     }
 
+    public void gerenciarChat()
+    {
+        try (Socket socket = new Socket(this.host, this.porta);
+             ClienteSocket clienteSocket = new ClienteSocket(socket))
+        {
+            Cliente cliente = new Cliente(clienteSocket);
+            Thread threadReceber = new Thread(cliente);
+            System.out.println("Conectado com Sucesso");
+            threadReceber.start();
+            cliente.enviarMensagem();
+
+        } catch (IOException e) {
+            System.out.println("Nao foi possivel conectar ao servidor"+e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
